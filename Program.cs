c94797e8d62cf4c7614092cf16eeed44e0f81ff1
kerem <kerembuyukhamurkar@gmail.com>
2023-04-16 15:30:33 +0300

    feat:kullanıcı girdileri eklendi

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi");
+            Console.WriteLine("ilk sayıyı giriniz");
+            double sayi1=double.Parse(Console.ReadLine());  
         }
     }
 }
